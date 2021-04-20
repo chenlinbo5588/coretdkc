53b56fc724955d4cdff45c3adb9978d0abf293ae
chenlinbo5588 <chenlinbo5588@163.com>
2021-04-21 00:02:51 +0800

    unit test

--- src/test/java/com/clb/utils/CalculatorTest.java
+++ src/test/java/com/clb/utils/CalculatorTest.java
@@ -1,7 +1,42 @@
 package com.clb.utils;
 
 
-public class CalculatorTest {
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
 
+import java.util.Collections;
+
+import static org.mockito.ArgumentMatchers.anyChar;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
+
+public class CalculatorTest {
+    private Calculator calculator;
+    private Operation mockOperation;
+
+    @Before
+    public void setup() {
+        mockOperation = Mockito.mock(Operation.class);
+        calculator =  new Calculator(Collections.
+                singletonList(mockOperation));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void throwExceptionWhenNoSuitableOperationFound() {
+        when(mockOperation.handles(anyChar())).thenReturn(false);
+        calculator.calculate(2, 2, '*');
+    }
+
+
+    @Test
+    public void shouldCallApplyMethodWhenSuitableOperationFound() {
+        when(mockOperation.handles(anyChar())).thenReturn(true);
+        when(mockOperation.apply(2, 2)).thenReturn(4);
+
+        calculator.calculate(2, 2, '*');
+        verify(mockOperation, times(1)).apply(2,2);
+    }
 
 }
--- src/test/java/com/clb/utils/MultiplicationTest.java
+++ src/test/java/com/clb/utils/MultiplicationTest.java
@@ -1,17 +1,27 @@
 package com.clb.utils;
 
 import com.clb.componet.Multiplication;
+import org.junit.Test;
 
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
+
+//基本单元测试
 public class MultiplicationTest {
 
     private final Multiplication addition = new Multiplication();
 
     @Test
     public void sholdMatchOperation(){
-        //assertThat(addition.handles('*')).isTrue();
-
-
+        assertThat(addition.handles('*')).isTrue();
+        assertThat(addition.handles('/')).isFalse();
     }
+
+
+    @Test
+    public void shouldCorrectlyApplyFormula(){
+        assertThat(addition.apply(2,2)).isEqualTo(4);
+        assertThat(addition.apply(12,10)).isEqualTo(120);
+    }
+
 }
