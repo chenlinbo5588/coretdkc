a06561fe2141c4ac42c348e10dfe3d195a910f41
chenlinbo5588 <chenlinbo5588@163.com>
2021-04-30 17:47:51 +0800

    单元测试

---
+++ src/test/java/com/clb/RiverWebApplicationTests.java
@@ -0,0 +1,70 @@
+package com.clb;
+
+import com.clb.utils.Calculator;
+import com.clb.utils.Operation;
+import org.hamcrest.Matchers;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.system.OutputCaptureRule;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Collection;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = RiverWebApplication.class)
+public class RiverWebApplicationTests {
+
+    @Rule
+    public OutputCaptureRule capture = new OutputCaptureRule();
+
+    @MockBean(name ="division")
+    private Operation mockOperation;
+
+    @Autowired
+    private Calculator calculator;
+
+    @Test
+    public void calculatorShouldHave3Operations() {
+        Object operations =
+                ReflectionTestUtils.getField(calculator, "operations");
+
+        assertThat((Collection) operations).hasSize(3);
+    }
+
+    @Test
+    public void mockDivision() {
+        when(mockOperation.handles('/')).thenReturn(true);
+        when(mockOperation.apply(14, 7)).thenReturn(2);
+
+        calculator.calculate(14,7, '/');
+        capture.expect(Matchers.containsString("14 / 7 = 2"));
+    }
+
+    @Test
+    public void doingMultiplicationShouldSucceed() {
+        calculator.calculate(12,13, '*');
+        capture.expect(Matchers.containsString("12 * 13 = 156"));
+    }
+
+    @Test
+    public void doingAdditionShouldSucceed() {
+        calculator.calculate(12,13, '+');
+        capture.expect(Matchers.containsString("12 + 13 = 25"));
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void doingDivisionShouldFail() {
+        calculator.calculate(12,13, '/');
+    }
+
+
+}
